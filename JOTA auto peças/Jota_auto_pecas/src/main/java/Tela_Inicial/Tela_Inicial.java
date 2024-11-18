d995c7d3a88a0d6bf57efb3683dc4a4da1d82ad9
PEDRO HENRIQUE AMANCIO DOS SANTOS ROSA <p.rosa@labcetec.net>
2024-11-18 20:08:28 -0300

    Pedro

--- JOTA auto peças/Jota_auto_pecas/src/main/java/Tela_Inicial/Tela_Inicial.java
+++ JOTA auto peças/Jota_auto_pecas/src/main/java/Tela_Inicial/Tela_Inicial.java
@@ -29,7 +29,7 @@ public class Tela_Inicial extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        Ida_CFuncionario = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
@@ -52,10 +52,10 @@ public class Tela_Inicial extends javax.swing.JFrame {
 
         jButton1.setText("Cadastro de carros");
 
-        jButton2.setText("Cadastro de funcionário");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        Ida_CFuncionario.setText("Cadastro de funcionário");
+        Ida_CFuncionario.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                Ida_CFuncionarioActionPerformed(evt);
             }
         });
 
@@ -141,7 +141,7 @@ public class Tela_Inicial extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(jButton4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
+                            .addComponent(Ida_CFuncionario, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
                             .addComponent(jButton7, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -162,7 +162,7 @@ public class Tela_Inicial extends javax.swing.JFrame {
                         .addGap(29, 29, 29)
                         .addComponent(jButton1)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton2)
+                        .addComponent(Ida_CFuncionario)
                         .addGap(20, 20, 20)
                         .addComponent(jButton3)
                         .addGap(18, 18, 18)
@@ -191,9 +191,13 @@ public class Tela_Inicial extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void Ida_CFuncionarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Ida_CFuncionarioActionPerformed
+        Cadastro_funcionario telFun = new Cadastro_funcionario();
+        telFun.setVisible(true);
+        this.dispose();      
+
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_Ida_CFuncionarioActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
@@ -240,8 +244,8 @@ public class Tela_Inicial extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Ida_CFuncionario;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
@@ -255,4 +259,10 @@ public class Tela_Inicial extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
+
+    private static class Cadastro_funcionario extends Tela_Inicial {
+
+        public Cadastro_funcionario() {
+        }
+    }
 }
