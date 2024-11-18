b098f0938726ca6c8117f2c028c8f4da656803e6
Lian Henrique <lian.h.silva@aluno.senai.br>
2024-11-18 20:27:34 -0300

    att

--- JOTA auto peças/Jota_auto_pecas/src/main/java/Tela_Inicial/Tela_Inicial.java
+++ JOTA auto peças/Jota_auto_pecas/src/main/java/Tela_Inicial/Tela_Inicial.java
@@ -4,6 +4,12 @@
  */
 package Tela_Inicial;
 
+import Cadastro_cliente.Cadastro_cliente;
+import Cadastro_funcionario.Frame_Cadastro_Funcionario;
+import Cadastro_pecas_lotes.Cadastro_pecas_lotes;
+import Cadastro_pedidos.Cadastro_pedidos;
+import Ordem_Servico.Ordem_Servico;
+
 /**
  *
  * @author f.silva
@@ -29,7 +35,7 @@ public class Tela_Inicial extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        Ida_CFuncionario = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
@@ -57,10 +63,10 @@ public class Tela_Inicial extends javax.swing.JFrame {
             }
         });
 
-        Ida_CFuncionario.setText("Cadastro de funcionário");
-        Ida_CFuncionario.addActionListener(new java.awt.event.ActionListener() {
+        jButton2.setText("Cadastro de funcionário");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                Ida_CFuncionarioActionPerformed(evt);
+                jButton2ActionPerformed(evt);
             }
         });
 
@@ -72,8 +78,18 @@ public class Tela_Inicial extends javax.swing.JFrame {
         });
 
         jButton4.setText("Ordem de serviço");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jButton5.setText("Cadastro de cliente");
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Logo.png"))); // NOI18N
         jLabel2.setText("jLabel2");
@@ -146,7 +162,7 @@ public class Tela_Inicial extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(jButton4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(Ida_CFuncionario, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
+                            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 195, Short.MAX_VALUE)
                             .addComponent(jButton7, javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -167,7 +183,7 @@ public class Tela_Inicial extends javax.swing.JFrame {
                         .addGap(29, 29, 29)
                         .addComponent(jButton1)
                         .addGap(18, 18, 18)
-                        .addComponent(Ida_CFuncionario)
+                        .addComponent(jButton2)
                         .addGap(20, 20, 20)
                         .addComponent(jButton3)
                         .addGap(18, 18, 18)
@@ -196,16 +212,18 @@ public class Tela_Inicial extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void Ida_CFuncionarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Ida_CFuncionarioActionPerformed
-        Cadastro_funcionario telFun = new Cadastro_funcionario();
-        telFun.setVisible(true);
-        this.dispose();      
-
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_Ida_CFuncionarioActionPerformed
+        Frame_Cadastro_Funcionario cadFunc = new Frame_Cadastro_Funcionario();
+        cadFunc.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        Cadastro_pedidos cadPed = new Cadastro_pedidos();
+        cadPed.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
@@ -215,8 +233,24 @@ public class Tela_Inicial extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        
+        Cadastro_pecas_lotes cadPecas = new Cadastro_pecas_lotes();
+        cadPecas.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        Ordem_Servico ordServ = new Ordem_Servico();
+        ordServ.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+        Cadastro_cliente cadCli = new Cadastro_cliente();
+        cadCli.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jButton5ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -254,8 +288,8 @@ public class Tela_Inicial extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Ida_CFuncionario;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
@@ -269,10 +303,4 @@ public class Tela_Inicial extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
-
-    private static class Cadastro_funcionario extends Tela_Inicial {
-
-        public Cadastro_funcionario() {
-        }
-    }
 }
