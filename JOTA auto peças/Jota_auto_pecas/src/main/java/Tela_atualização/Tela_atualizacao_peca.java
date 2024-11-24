513460991011adb5970cbc9ea381e3c3aaed37cb
PedrohAmancio <pedro.h.rosa10@aluno.senai.br>
2024-11-24 01:38:46 -0300

    Atualizacao relacionadas a pecas - Pedro

--- JOTA auto peças/Jota_auto_pecas/src/main/java/Tela_atualização/Tela_atualizacao_peca.java
+++ JOTA auto peças/Jota_auto_pecas/src/main/java/Tela_atualização/Tela_atualizacao_peca.java
@@ -41,8 +41,10 @@ public class Tela_atualizacao_peca extends javax.swing.JFrame {
         jlbl_nome_peca = new javax.swing.JLabel();
         Sai_Att_pecas = new javax.swing.JButton();
         jbutton_salvar = new javax.swing.JButton();
-        jfield_id_peca = new javax.swing.JTextField();
-        jfield_qtd_pecas = new javax.swing.JTextField();
+        Novo_preço = new javax.swing.JTextField();
+        jfield_ID_pecas = new javax.swing.JTextField();
+        jfield_QTD_peca = new javax.swing.JTextField();
+        jlbl_nome_peca1 = new javax.swing.JLabel();
         jfield_nome_peca = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -60,17 +62,17 @@ public class Tela_atualizacao_peca extends javax.swing.JFrame {
         jLabel1.setBackground(new java.awt.Color(255, 255, 255));
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("ID peça");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 120, -1, -1));
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 220, -1, -1));
 
         jlbl_qtd_pecas.setBackground(new java.awt.Color(255, 255, 255));
         jlbl_qtd_pecas.setForeground(new java.awt.Color(255, 255, 255));
         jlbl_qtd_pecas.setText("Nova quantidade");
-        jPanel1.add(jlbl_qtd_pecas, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 170, -1, -1));
+        jPanel1.add(jlbl_qtd_pecas, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 120, -1, -1));
 
         jlbl_nome_peca.setBackground(new java.awt.Color(255, 255, 255));
         jlbl_nome_peca.setForeground(new java.awt.Color(255, 255, 255));
-        jlbl_nome_peca.setText("Nome");
-        jPanel1.add(jlbl_nome_peca, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 220, -1, -1));
+        jlbl_nome_peca.setText("Novo preço");
+        jPanel1.add(jlbl_nome_peca, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 270, -1, -1));
 
         Sai_Att_pecas.setText("Sair");
         Sai_Att_pecas.setBorder(null);
@@ -91,8 +93,14 @@ public class Tela_atualizacao_peca extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jbutton_salvar, new org.netbeans.lib.awtextra.AbsoluteConstraints(950, 570, 80, 30));
-        jPanel1.add(jfield_id_peca, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 220, 295, -1));
-        jPanel1.add(jfield_qtd_pecas, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 120, 295, -1));
+        jPanel1.add(Novo_preço, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 270, 295, -1));
+        jPanel1.add(jfield_ID_pecas, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 220, 295, -1));
+        jPanel1.add(jfield_QTD_peca, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 120, 295, -1));
+
+        jlbl_nome_peca1.setBackground(new java.awt.Color(255, 255, 255));
+        jlbl_nome_peca1.setForeground(new java.awt.Color(255, 255, 255));
+        jlbl_nome_peca1.setText("Nome");
+        jPanel1.add(jlbl_nome_peca1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 170, -1, -1));
         jPanel1.add(jfield_nome_peca, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 170, 295, -1));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 612));
@@ -112,12 +120,14 @@ public class Tela_atualizacao_peca extends javax.swing.JFrame {
             
             conexao = DriverManager.getConnection(url, usuario, senha);
             
-            String sql = "UPDATE pecas SET qtd_pecas = ?, nome_pecas = ? WHERE id_pecas = ?";
+            String sql = "UPDATE pecas SET qtd_pecas = ?, nome_pecas = ?, preco_peca = ? WHERE id_pecas = ?";
             
             statement = conexao.prepareStatement(sql);
-            statement.setString(1, jfield_qtd_pecas.getText());
+            statement.setString(1, jfield_QTD_peca.getText());
             statement.setString(2, jfield_nome_peca.getText());
-            statement.setString(3, jfield_id_peca.getText());
+            statement.setString(3, jfield_ID_pecas.getText());
+            statement.setString(4, Novo_preço.getText());
+
             
             statement.executeUpdate();
             
@@ -126,9 +136,9 @@ public class Tela_atualizacao_peca extends javax.swing.JFrame {
             System.out.println("Deu esse erro aqui ô: " + e.getMessage());
         }
         
-        jfield_qtd_pecas.setText(null);
-        jfield_nome_peca.setText(null);
-        jfield_id_peca.setText(null);
+        jfield_ID_pecas.setText(null);
+        jfield_QTD_peca.setText(null);
+        Novo_preço.setText(null);
     }//GEN-LAST:event_jbutton_salvarActionPerformed
 
     private void Sai_Att_pecasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Sai_Att_pecasActionPerformed
@@ -175,15 +185,17 @@ public class Tela_atualizacao_peca extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField Novo_preço;
     private javax.swing.JButton Sai_Att_pecas;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton jbutton_salvar;
-    private javax.swing.JTextField jfield_id_peca;
+    private javax.swing.JTextField jfield_ID_pecas;
+    private javax.swing.JTextField jfield_QTD_peca;
     private javax.swing.JTextField jfield_nome_peca;
-    private javax.swing.JTextField jfield_qtd_pecas;
     private javax.swing.JLabel jlbl_atualizao_dados;
     private javax.swing.JLabel jlbl_nome_peca;
+    private javax.swing.JLabel jlbl_nome_peca1;
     private javax.swing.JLabel jlbl_qtd_pecas;
     // End of variables declaration//GEN-END:variables
 }
